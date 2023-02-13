34c58aae220f9341ee80ad5332a1f8621992dc26
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2023-02-13 14:40:06 +0930

    assort-descendants

--- find_clone_images/__init__.py
+++ find_clone_images/__init__.py
@@ -1,6 +1,6 @@
 
 from image_hashing import dhash_z_transformed as hash_image
-from os import listdir, path, mkdir, rename
+from os import listdir, path, mkdir, rename, walk
 
 # for every file in the directory, hash it, and set the filepath as the value, and the hash as the key
 # if any files match an already existing one, move both images into a folder
@@ -12,7 +12,7 @@ def isValidFileType(file):
 			return True
 	return False
 
-def assort_clones(search_directory, clone_directory, move_original_with_clones=False):
+def assort_directory(search_directory, clone_directory, move_original_with_clones=False):
 	if not path.exists(clone_directory):
 		mkdir(clone_directory)
 
@@ -37,6 +37,36 @@ def assort_clones(search_directory, clone_directory, move_original_with_clones=F
 			print("Skipped error; ", e)
 			pass
 
+def assort_descendants(parent_directory, clone_directory, move_original_with_clones=False):
+	if not path.exists(clone_directory):
+		mkdir(clone_directory)
+
+	hash_dict = { }
+	for root, dirs, files in walk(parent_directory):
+		for file in files:
+			if not isValidFileType(file):
+				continue
+			try:
+				hashed_result = hash_image(path.join(root, file))
+				if hashed_result in hash_dict.keys():
+					# move original clone to folder
+					original_path = hash_dict.get(hashed_result)
+					if move_original_with_clones and path.exists( original_path ):
+						_, tail = path.split( original_path )
+						rename(original_path, path.join(clone_directory, tail))
+					# move new clone to folder
+					rename( path.join(root, file), path.join(clone_directory, file) )
+				else:
+					# add to hash dict
+					hash_dict[hashed_result] = path.join(root, file)
+			except Exception as e:
+				print("Skipped error; ", e)
+				pass
+
 search_directory = input("Input the filepath to search; ")
+search_type = input("Input 1 for direct children of parent, otherwise anything else for descendants of parent.")
 clone_directory = input("Input a folder name for any clone images; ")
-assort_clones(search_directory, path.join(search_directory, "clones"))
+if search_type == "1":
+	assort_directory(search_directory, path.join(search_directory, clone_directory))
+else:
+	assort_descendants(search_directory, path.join(search_directory, clone_directory))
